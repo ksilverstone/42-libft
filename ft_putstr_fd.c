e9417701ca3939feb85783f7524acf2d0c2c7a0c
Kerem Emre Gümüştaş <kerem@Kerem-Dizustu-Bilgisayar.local>
2024-11-03 15:18:13 +0300

    %100 Success

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: kgumusta <kgumusta@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 13:53:17 by kgumusta          #+#    #+#             */
-/*   Updated: 2024/11/02 02:07:09 by kgumusta         ###   ########.fr       */
+/*   Updated: 2024/11/03 15:17:16 by kgumusta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	ft_putstr_fd(char *s, int fd)
 {
+	if (s == NULL)
+		return ;
 	while (*s)
 	{
 		write(fd, s, 1);
